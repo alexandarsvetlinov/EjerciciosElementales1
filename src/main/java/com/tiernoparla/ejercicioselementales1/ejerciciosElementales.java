12d82794ee016c516be5c5009faa7ef58617ac20
alexandarsvetlinov <alexandarsvetlinov.ivanov@tiernoparla.es>
2022-09-27 20:13:40 +0200

    primera version

--- src/main/java/com/tiernoparla/ejercicioselementales1/ejerciciosElementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales1/ejerciciosElementales.java
@@ -4,6 +4,10 @@ package com.tiernoparla.ejercicioselementales1;
 
 public class ejerciciosElementales {
     public static void main(String[] args) {
-        System.out.println("hola");
+        
+        // print nums pares del 1 al 10
+        for(int i = 2; i < 11; i = i+2){
+            System.out.println(i);
+        }
     }
 }
