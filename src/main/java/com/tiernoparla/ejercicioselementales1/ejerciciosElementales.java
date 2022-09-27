273939b24b989b4494e84de01aad7a209b3620ed
alexandarsvetlinov <alexandarsvetlinov.ivanov@tiernoparla.es>
2022-09-27 18:55:16 +0200

    añado main class

---
+++ src/main/java/com/tiernoparla/ejercicioselementales1/ejerciciosElementales.java
@@ -0,0 +1,9 @@
+
+package com.tiernoparla.ejercicioselementales1;
+
+
+public class ejerciciosElementales {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
